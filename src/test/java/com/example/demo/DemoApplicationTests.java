da2cc80c69dfee823aca05db26d969c3e54b855a
Islem <kallouemedislem@gmail.com>
2025-04-18 09:58:42 +0100

    First commit“

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,10 @@
-package com.example.demo;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-}
+    @Test
+    void simpleTest() {
+        boolean expected = true;
+        boolean actual = true;
+        assertEquals(expected, actual);
